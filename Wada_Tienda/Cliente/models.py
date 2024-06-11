8f93023e0b6e8e5360d2b62f6271c8f788c087bc
dantemolina <dantemolina00@gmail.com>
2024-06-10 21:42:28 -0300

    creacion de apps

--- Wada_Tienda/Cliente/models.py
+++ Wada_Tienda/Cliente/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=100, unique=True)
+    
+
+class Cliente(models.Model):
+    nombre = models.CharField(max_length=200)
+    apellido = models.CharField(max_length=200)
+    correo = models.EmailField(max_length=250)
+    telefono = models.DecimalField(max_digits=20)
+    ciudad_origen_id = models.ForeignKey(Ciudad, on_delete=models.SET_NULL, null=True, blank=True)
+
